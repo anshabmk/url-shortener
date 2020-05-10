d3d62e24707e039762cfcf9c526df5f2564058d4
Anshab M K <anshabmk@live.com>
2020-05-10 17:07:38 +0530

    Adds validation for uniqueness of token

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,5 +1,6 @@
 class ShortLink < ApplicationRecord
-  validates :token, presence: true, 
+  validates :token, presence: true,
+                    uniqueness: true,
                     length: { is: 5 }, 
                     format: { with: /[a-zA-Z0-9]{5}/ }
 end
