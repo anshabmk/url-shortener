62c7ee83f6985f54fb2fcd2cb58bbd2a09d6d8dd
Anshab M K <anshabmk@live.com>
2020-05-10 17:20:09 +0530

    Use let to define random_token and random_url for reuse

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
+  let(:random_url) { Faker::Internet.url }
+  let(:random_token) { Faker::Alphanumeric.alphanumeric(number: 5) }
+
   context 'token' do
     it 'should not save without a token' do
-      short_link = ShortLink.new(long_url: Faker::Internet.url)
+      short_link = ShortLink.new(long_url: random_url)
   
       expect(short_link.save).to be false
     end
 
     it 'should not save if the length of the token is not 5' do
       short_link = ShortLink.new(
-        long_url: Faker::Internet.url, 
+        long_url: random_url,
         token: Faker::Alphanumeric.alphanumeric(number: Random.rand(6..10))
       )
 
@@ -18,35 +21,21 @@ RSpec.describe ShortLink, type: :model do
     end
 
     it 'should not contain anything other than alphanumeric as token' do
-      short_link = ShortLink.new(
-        long_url: Faker::Internet.url,
-        token: 'abc1@'
-      )
+      short_link = ShortLink.new(long_url: random_url, token: 'abc1@')
 
       expect(short_link.save).to be false
     end
 
     it 'should not save if token is not unique' do
-      token = Faker::Alphanumeric.alphanumeric(number: 5)
-
-      short_link1 = ShortLink.create!(
-        long_url: Faker::Internet.url,
-        token: token
-      )
-
-      short_link2 = ShortLink.new(
-        long_url: Faker::Internet.url,
-        token: token
-      )
+      token = random_token
+      short_link1 = ShortLink.create!(long_url: random_url, token: token)
+      short_link2 = ShortLink.new(long_url: random_url, token: token)
 
       expect(short_link2.save).to be false
     end
 
     it 'should save if valid token is given' do
-      short_link = ShortLink.new(
-        long_url: Faker::Internet.url,
-        token: Faker::Alphanumeric.alphanumeric(number: 5)
-      )
+      short_link = ShortLink.new(long_url: random_url, token: random_token)
 
       expect(short_link.save).to be true
     end
