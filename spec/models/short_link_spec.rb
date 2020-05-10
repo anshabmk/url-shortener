be6b916fa2f9af9f0ca321f3528e53e8cc87dc07
Anshab M K <anshabmk@live.com>
2020-05-10 17:00:57 +0530

    Adds test for token characters format

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
   context 'token' do
-    it 'should not save the record without a token' do
+    it 'should not save without a token' do
       short_link = ShortLink.new(long_url: Faker::Internet.url)
   
       expect(short_link.save).to be false
     end
 
-    it 'should not save the record if the length of the token is not 5' do
+    it 'should not save if the length of the token is not 5' do
       short_link = ShortLink.new(
         long_url: Faker::Internet.url, 
         token: Faker::Alphanumeric.alphanumeric(number: Random.rand(6..10))
+      )
+
+      expect(short_link.save).to be false
+    end
+
+    it 'should not contain anything other than alphanumeric as token' do
+      short_link = ShortLink.new(
+        long_url: Faker::Internet.url,
+        token: 'abc1@'
       )
 
       expect(short_link.save).to be false
