172464ef2da87bed73eaab91552f3cfeeec4d120
Anshab M K <anshabmk@live.com>
2020-05-10 23:01:46 +0530

    Changes default resource route of show short_link/:id to /:token

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :short_links, only: %i[new create show]
+  resources :short_links, only: %i[new create]
+
+  get '/:token', to: 'short_links#show', as: 'short_link'
 end
