675629dff9859bcfe2344783724ffda51f9794c8
Anshab M K <anshabmk@live.com>
2020-05-10 16:49:42 +0530

    Adds test for validating the length of the token

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
   context 'token' do
-    it 'should validate presence of token' do
+    it 'should not save the record without a token' do
       short_link = ShortLink.new(long_url: Faker::Internet.url)
   
+      expect(short_link.save).to be false
+    end
+
+    it 'should not save the record if the length of the token is not 5' do
+      short_link = ShortLink.new(
+        long_url: Faker::Internet.url, 
+        token: Faker::Alphanumeric.alphanumeric(number: Random.rand(6..10))
+      )
+
       expect(short_link.save).to be false
     end
   end
