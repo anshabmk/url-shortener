4580b44c73c7ed9d7f6a3560f6e6161a53e45d78
Anshab M K <anshabmk@live.com>
2020-05-10 17:23:48 +0530

    Validates presence of long_url

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,6 +1,8 @@
 class ShortLink < ApplicationRecord
   validates :token, presence: true,
                     uniqueness: true,
-                    length: { is: 5 }, 
+                    length: { is: 5 },
                     format: { with: /[a-zA-Z0-9]{5}/ }
+  
+  validates :long_url, presence: true
 end
