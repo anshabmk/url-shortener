ad445b7ba03210516d4d510c27397a8340aab112
Anshab M K <anshabmk@live.com>
2020-05-11 13:51:32 +0530

    Respond to js in both cases, successful creation and failure

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -2,15 +2,9 @@ class ShortLinksController < ApplicationController
   def new; end
 
   def create
-    @short_link = ShortLink.new(long_url: params[:short_link][:long_url])
+    @short_link = ShortLink.create(long_url: params[:short_link][:long_url])
 
-    respond_to do |format|
-      if @short_link.save
-        format.js
-      else
-        format.html { render action: 'new' }
-      end
-    end
+    respond_to { |format| format.js }
   end
 
   def show
