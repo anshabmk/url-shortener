66b9455c1507f8c67b4bc5d7c6f30ba4a6034b61
Anshab M K <anshabmk@live.com>
2020-05-11 11:25:05 +0530

    Adds test for creating a short link

--- spec/requests/short_links_request_spec.rb
+++ spec/requests/short_links_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "ShortLinks", type: :request do
+  it 'creates a short link' do
+    get '/short_links/new'
+    expect(response).to render_template(:new)
 
+    post '/short_links', xhr: true, params: { short_link: { long_url: Faker::Internet.url } }
+    expect(response).to render_template(:create)
+  end
 end
