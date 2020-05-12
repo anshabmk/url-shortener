67bc922a5186f728c8a678e65d84128021abc383
Anshab M K <anshabmk@live.com>
2020-05-13 05:28:22 +0530

    Adds tests for http success and render index template

--- spec/requests/stats_request_spec.rb
+++ spec/requests/stats_request_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Stats", type: :request do
+RSpec.describe 'Stats', type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/stats/index"
+  describe 'GET /stats' do
+    it 'returns http success' do
+      get '/stats'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders index template' do
+      get '/stats'
+      expect(response).to render_template(:index)
     end
   end
 
