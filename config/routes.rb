725ad5fa1c9447c66ab173c5b5e55126fe88c2d3
Anshab M K <anshabmk@live.com>
2020-05-13 00:34:17 +0530

    Adds index resource for stats

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'short_links#new'
 
   resources :short_links, only: %i[new create]
+  resources :stats, only: %i[index]
 
   get '/:token', to: 'short_links#show', as: 'short_link'
 end
