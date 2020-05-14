52dbbe7763cd81c49f1669b836faf9aa2961c410
Anshab M K <anshabmk@live.com>
2020-05-14 07:10:12 +0530

    Adds pagination to index

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,15 @@
 class StatsController < ApplicationController
+  PAGE_START = '1'
+  PER_PAGE = '5'
+
   def index
+    @page = (params[:page] || PAGE_START).to_i
+    @per_page = (params[:per_page] || PER_PAGE).to_i
+    @total_count = ShortLink.count
+    @offset = @per_page * (@page - 1)
+
     @short_links = ShortLink.order(created_at: :desc)
+                            .offset(@offset)
+                            .limit(@per_page)
   end
 end
