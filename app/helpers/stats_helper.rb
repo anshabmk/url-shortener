e5966aa68574836601b66385b9ba37dbc8c38338
Anshab M K <anshabmk@live.com>
2020-05-14 07:10:50 +0530

    Adds helper methods for pagination

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -2,4 +2,16 @@ module StatsHelper
   def top_countries(short_link)
     short_link.top_countries_with_count(3).keys.join(', ')
   end
+
+  def previous_page
+    return unless @page > 1
+
+    @page - 1
+  end
+
+  def next_page
+    return unless (@total_count / @per_page.to_f) > @page
+
+    @page + 1
+  end
 end
